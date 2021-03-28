929aae455139c0807600f8cbb9a854607e949b10
Robert <robert.witts.rmw@gmail.com>
2021-03-28 12:57:43 +0100

    Restructured, added comments, made new crater code work with larger nukes

--- True-Nukes_0.1.6/control.lua
+++ True-Nukes_0.1.6/control.lua
@@ -8,6 +8,7 @@ script.on_init(function()
 end)
 
 local function moveBlast(i,blast,pastEHits)
+	-- Compute the number of regions we move the blast in
 	local regNum = 8
 	if(blast.r<=500 or not blast.doItts) then
 		regNum = 8
@@ -18,6 +19,7 @@ local function moveBlast(i,blast,pastEHits)
 	else
 		regNum = 96
 	end
+	-- Do we need to wait a while (we might need to if the simulated blast is going faster than expected) 
 	blast.ittframe = blast.ittframe+1
 	if(blast.itt > regNum and blast.ittframe >=8) then
 		blast.r = blast.r + blast.speed
@@ -36,6 +38,7 @@ local function moveBlast(i,blast,pastEHits)
 	
 	local area = {{}, {}}
 
+	-- Some hard-coded regions for small blasts
 	local regions = {{{center.x-blast.r/2-sideOffset, center.y+(blast.r-extraSpace)*0.86603-0.5}, {center.x+blast.r/2+sideOffset, center.y+blast.r+1}}, 
 		 		 {{center.x-blast.r/2-sideOffset, center.y-blast.r}, {center.x+blast.r/2+sideOffset, center.y-(blast.r-extraSpace)*0.86603+0.5}}, 
 				 {{center.x+(blast.r-extraSpace)*0.86603-0.5, center.y-blast.r/2-sideOffset}, {center.x+blast.r+1, center.y+blast.r/2+sideOffset}}, 
@@ -49,7 +52,7 @@ local function moveBlast(i,blast,pastEHits)
 	if(blast.r<=500 or not blast.doItts) then
 		area = regions[blast.itt]
 	else
-
+		-- otherwise compute the regions for large area blast-waves
 		local reg = blast.itt % (regNum/4)
 		local currentQuadrant = (math.floor(blast.itt/(regNum/4)))%4
 		local angleUnit = 2*3.14159/regNum
@@ -110,6 +113,7 @@ local function moveBlast(i,blast,pastEHits)
 	eHits = eHits + #entities
 	--game.players[1].print(math.floor(blast.itt/6) .. " {" .. area[1][1] .. ", " .. area[1][2] .. "}, {" .. area[2][1] .. ", " .. area[2][2] .. "}")
 	for _,entity in pairs(entities) do
+		-- do blast damage - reduced for rails, belts, land mines and flying vehicles, as this makes some sense, and trees in order to leave some alive
 		if (entity.valid and entity.position) then
 			local xdif = entity.position.x-center.x
 			local ydif = entity.position.y-center.y
@@ -140,6 +144,7 @@ local function moveBlast(i,blast,pastEHits)
 					damage = math.random(damage/2, damage*2)
 				end
 				if(t=="tree") then
+					-- If a tree is destroyed, don't bother boing particle effects, just destroy it - huge performance savings
 					if((((not entity.prototype.resistances) or not entity.prototype.resistances.fire) and entity.health<damage) or (entity.prototype.resistances and entity.prototype.resistances.fire and entity.health<(damage-entity.prototype.resistances.explosion.decrease)*(1-entity.prototype.resistances.explosion.percent))) then
 						local destPos = entity.position
 						entity.destroy()
@@ -150,6 +155,7 @@ local function moveBlast(i,blast,pastEHits)
 				else
 					entity.damage(damage, "enemy","explosion")
 				end
+				-- For thermobarics, with the blast wave carrying the fire
 				if blast.fire and entity.valid and (entity.type == "unit" or entity.type == "car" or entity.type == "spider-vehicle") then
 					local fireShield = nil
 					if entity.grid then
@@ -179,6 +185,7 @@ local function moveBlast(i,blast,pastEHits)
 		end
 	end
 
+	-- For thermobarics, start all the required fires
 	if(blast.fire) then
 		local area = regions[blast.itt]
 		local tiles = surface.find_tiles_filtered{area=area}
@@ -203,6 +210,7 @@ local function moveBlast(i,blast,pastEHits)
 			end
 		end
 	end
+	-- We want to do more regions this frame if the ones we have covered contain very few entities (such as it they are unloaded)
 	if (blast.ittframe>=8) then
 		if(blast.itt == regNum) then
 			blast.r = blast.r + blast.speed
@@ -220,6 +228,7 @@ local function moveBlast(i,blast,pastEHits)
 end
 
 local function nukeBuildingDetonate(building)
+	-- A nuke building just launches an artillery shell at itself, much easier than an entire seperate detonation system
 	if(building.get_recipe().name == "megaton-detonation") then
 		building.surface.create_entity{name="TN-really-huge-atomic-artillery-projectile", position={building.position.x-1, building.position.y}, target=building, speed=100, max_range=1, force=building.force}
 	elseif(building.get_recipe().name == "100kiloton-detonation") then
@@ -250,6 +259,7 @@ local function tickHandler(event)
 				if(not building.get_output_inventory().is_empty()) then
 					nukeBuildingDetonate(building)
 				elseif (building.crafting_progress > 0 and building.crafting_progress < 0.01) then
+					-- Force map loading when a nuke is set up
 					if(building.get_recipe().name == "megaton-detonation") then
 	 					building.surface.request_to_generate_chunks(building.position, 3200/32)
 					elseif(building.get_recipe().name == "100kiloton-detonation") then
@@ -267,10 +277,74 @@ end
 script.on_event(defines.events.on_tick, tickHandler);
 
 
+local function nukeTileChanges(position, check_craters, surface_index, crater_internal_r, crater_external_r)
+	local tileTable = {}
+
+	local is_waterfilled = crater_external_r<=8
+	if (check_craters) then
+		local edge_water_count = 0
+		local edge_water_threshold = 0.1 -- Threshold of proportion of crater edge touching water for crater to fill with water
+
+		for _,v in pairs(game.surfaces[surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
+			cur_tile = game.surfaces[surface_index].get_tile(v.position)
+			if math.sqrt((cur_tile.position.x - position.x) ^ 2 + (cur_tile.position.y - position.y) ^ 2) > crater_external_r - 1 then
+				if cur_tile.name == 'water' or cur_tile.name == 'deepwater' then
+					edge_water_count = edge_water_count + 1
+				end
+			end
+		end
+
+		if edge_water_count / (2 * math.pi * crater_external_r) > edge_water_threshold then
+			is_waterfilled = true
+		end
+
+		-- mandelbrodt - Moved from above to check if crater is next to water before appending to waitingNukes
+		if crater_internal_r>0 and settings.global["crater-water-filling"].value and not is_waterfilled then
+			table.insert(global.waitingNukes, {t = 0, pos = position, d = crater_internal_r, s = surface_index})
+		end
+	end
+	if ((not check_craters) or not is_waterfilled) then
+		for _,v in pairs(game.surfaces[surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
+			table.insert(tileTable, {name = "nuclear-ground", position = v.position})
+		end
+	else
+		-- mandelbrodt - If crater touches (non-shallow/mud) water, fill crater with 'water' tiles
+		for _,v in pairs(game.surfaces[surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
+			cur_tile = game.surfaces[surface_index].get_tile(v.position)
+			if not (cur_tile.name == 'water' or cur_tile.name == 'deepwater') then
+				if((v.position.x-position.x)*(v.position.x-position.x)+(v.position.y-position.y)*(v.position.y-position.y) <= crater_internal_r*crater_internal_r) then
+					table.insert(tileTable, {name = 'water', position = v.position})
+				else
+					table.insert(tileTable, {name = 'nuclear-ground', position = v.position})
+				end
+			end
+		end
+	end
+
+	-- do the noise around the craters
+	if (crater_external_r>8 and settings.global["nuke-crater-noise"].value) then
+		for num=0,3 do
+			local slice_w = (math.floor(crater_external_r/50)+1)
+			for ang=0,math.ceil(3.1416*2*crater_external_r*slice_w*4/(num*num+1)) do
+				local dist = math.floor(math.random(num*slice_w, slice_w+num*slice_w))
+				local offset = math.random()
+
+				noise_pos = {position.x+math.floor((dist+crater_external_r-1)*math.sin(ang+offset)+0.5), position.y+math.floor((dist+crater_external_r-1)*math.cos(ang+offset)+0.5)}
+				cur_tile = game.surfaces[1].get_tile(noise_pos)
+				-- mandelbrodt - Only replace non-water ('water' & 'deepwater') tiles with 'nuclear-ground', if crater is water-filled
+				if ((not check_craters) or not (is_waterfilled) or not (cur_tile.name == 'water' or cur_tile.name == 'deepwater')) then
+					table.insert(tileTable, {name = "nuclear-ground", position = noise_pos})
+				end
+			end
+		end
+	end
+
+	 game.surfaces[surface_index].set_tiles(tileTable)
+end
 
 
-
-local function atomic_weapon_hit(event, crater_internal_r, crater_external_r, fireball_r, fire_outer_r, blast_max_r, tree_fire_max_r, thermal_max_r, load_r, visable_r, flame_proportion, create_small_fires)
+local function atomic_weapon_hit(event, crater_internal_r, crater_external_r, fireball_r, fire_outer_r, blast_max_r, tree_fire_max_r, thermal_max_r, load_r, visable_r, flame_proportion, create_small_fires, check_craters)
+	 -- find forces, positions, etc.
 	 local force
 	 local position = event.target_position
 	 if(not position) then
@@ -285,12 +359,14 @@ local function atomic_weapon_hit(event, crater_internal_r, crater_external_r, fi
 		global.waitingNukes = {}
 	 end
 
+	 -- force the map to generate (should be reasonably quick as it is pre-loaded)
 	 game.surfaces[event.surface_index].request_to_generate_chunks(position, load_r/32)
 	 game.surfaces[event.surface_index].force_generate_chunk_requests()
 
 	 for _,f in pairs(game.forces) do
 		f.chart(game.surfaces[event.surface_index], {{position.x-visable_r,position.y-visable_r},{position.x+visable_r,position.y+visable_r}})
 	 end
+	 -- kill things in the fireball
 	 for _,v in pairs(game.surfaces[event.surface_index].find_entities_filtered{position=position, radius=fireball_r}) do
 		if(v.valid and (not (string.match(v.type, "ghost")))) then
 			if v.type=="tree" or not v.die(nil) then
@@ -298,73 +374,23 @@ local function atomic_weapon_hit(event, crater_internal_r, crater_external_r, fi
 			end
 		end
 	 end
+	 -- destroy decoratives in the fireball
 	 for _,v in pairs(game.surfaces[event.surface_index].find_decoratives_filtered{area = {{position.x-fireball_r, position.y-fireball_r}, {position.x+fireball_r, position.y+fireball_r}}}) do
 		if((v.position.x-position.x)*(v.position.x-position.x)+(v.position.y-position.y)*(v.position.y-position.y)<=fireball_r*fireball_r) then
 			game.surfaces[event.surface_index].destroy_decoratives{position = v.position};
 		end
 	 end
+	 -- make sure everything is dead in the fireball
 	 for _,v in pairs(game.surfaces[event.surface_index].find_entities_filtered{position=position, radius=fireball_r}) do
-		if(not (string.match(v.type, "ghost"))) then
+		if(v.valid and not (string.match(v.type, "ghost"))) then
 			v.die(nil);
 			v.destroy();
 		end
 	 end
-	 local tileTable = {}
 
-	local is_waterfilled = false
-	local edge_water_count = 0
-	local edge_water_threshold = 0.1 -- Threshold of proportion of crater edge touching water for crater to fill with water
+	 nukeTileChanges(position, check_craters, event.surface_index, crater_internal_r, crater_external_r)
 
-	for _,v in pairs(game.surfaces[event.surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
-		cur_tile = game.surfaces[1].get_tile(v.position)
-		if math.sqrt((cur_tile.position.x - position.x) ^ 2 + (cur_tile.position.y - position.y) ^ 2) > crater_external_r - 1 then
-			if cur_tile.name == 'water' or cur_tile.name == 'deepwater' then
-				edge_water_count = edge_water_count + 1
-			end
-		end
-	end
-
-	if edge_water_count / (2 * math.pi * crater_external_r) > edge_water_threshold then
-		is_waterfilled = true
-	end
-
-	-- mandelbrodt - Moved from above to check if crater is next to water before appending to waitingNukes
-	if crater_internal_r>0 and settings.global["crater-water-filling"].value and not is_waterfilled then
-		table.insert(global.waitingNukes, {t = 0, pos = position, d = crater_internal_r, s = event.surface_index})
-	end
-
-	if not is_waterfilled then
-		for _,v in pairs(game.surfaces[event.surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
-			table.insert(tileTable, {name = "nuclear-ground", position = v.position})
-		end
-	else
-		-- mandelbrodt - If crater touches (non-shallow/mud) water, fill crater with 'water' tiles
-		for _,v in pairs(game.surfaces[event.surface_index].find_tiles_filtered{position=position, radius=crater_external_r}) do
-			cur_tile = game.surfaces[1].get_tile(v.position)
-			if not (cur_tile.name == 'water' or cur_tile.name == 'deepwater') then
-				table.insert(tileTable, {name = 'water', position = v.position})
-			end
-		end
-	end
-
-	if (crater_external_r>8 and settings.global["nuke-crater-noise"].value) then
-		for num=0,3 do
-			local slice_w = (math.floor(crater_external_r/50)+1)
-			for ang=0,math.ceil(3.1416*2*crater_external_r*slice_w*4/(num*num+1)) do
-				local dist = math.floor(math.random(num*slice_w, slice_w+num*slice_w))
-				local offset = math.random()
-
-				noise_pos = {position.x+math.floor((dist+crater_external_r-1)*math.sin(ang+offset)+0.5), position.y+math.floor((dist+crater_external_r-1)*math.cos(ang+offset)+0.5)}
-				cur_tile = game.surfaces[1].get_tile(noise_pos)
-				-- mandelbrodt - Only replace non-water ('water' & 'deepwater') tiles with 'nuclear-ground', if crater is water-filled
-				if not (is_waterfilled) or not (cur_tile.name == 'water' or cur_tile.name == 'deepwater') then
-					table.insert(tileTable, {name = "nuclear-ground", position = noise_pos})
-				end
-			end
-		end
-	end
-
-	 game.surfaces[event.surface_index].set_tiles(tileTable)
+	 -- light fires as nessesary
 	 if(flame_proportion>0) then
 		 for _,v in pairs(game.surfaces[event.surface_index].find_tiles_filtered{position=position, radius=fire_outer_r}) do
 			local rand = math.random(0, fire_outer_r)
@@ -380,12 +406,14 @@ local function atomic_weapon_hit(event, crater_internal_r, crater_external_r, fi
 			game.surfaces[event.surface_index].create_entity{name="thermobaric-wave-fire",position={position.x+dist*math.cos(angle), position.y+dist*math.sin(angle)}}
 		end
 	 end
+	 -- do thermal heat-wave damage
 	 for _,v in pairs(game.surfaces[event.surface_index].find_entities_filtered{position=position, radius=thermal_max_r}) do
 		if(v.valid and not (v.prototype.max_health == 0)) then
 			local distSq = (v.position.x-position.x)*(v.position.x-position.x)+(v.position.y-position.y)*(v.position.y-position.y)
 			if(distSq>fireball_r) then
 				local damage = thermal_max_r*thermal_max_r/distSq*10	
 				if(v.type=="tree") then
+	 				-- efficient tree handling
 					if(math.random(0, 100)<1) then
 						game.surfaces[event.surface_index].create_entity{name="fire-flame-on-tree",position=v.position, initial_ground_flame_count=1+math.min(254,thermal_max_r*thermal_max_r/distSq)}
 					end
@@ -482,37 +510,37 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
   elseif(event.effect_id=="Thermobaric Weapon hit large") then
 	 thermobaric_weapon_hit(event, 9, 120, 100, 120, 100);
   elseif(event.effect_id=="Atomic Weapon hit 0.1t") then
-	 atomic_weapon_hit(event, 0, 1, 1, 3, 30, 15, 30, 15, 15, 1, true);
+	 atomic_weapon_hit(event, 0, 1, 1, 3, 30, 15, 30, 15, 15, 1, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 60, 100, 200, 700, 10, 0.06);
   elseif(event.effect_id=="Atomic Weapon hit 0.5t") then
-	 atomic_weapon_hit(event, 0, 3, 3, 5, 50, 25, 30, 30, 20, 1, true);
+	 atomic_weapon_hit(event, 0, 3, 3, 5, 50, 25, 30, 30, 20, 1, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 80, 150, 300, 1000, 20, 0.12);
   elseif(event.effect_id=="Atomic Weapon hit 2t") then
-	 atomic_weapon_hit(event, 0, 5, 5, 15, 80, 50, 100, 100, 50, 2, true);
+	 atomic_weapon_hit(event, 0, 5, 5, 15, 80, 50, 100, 100, 50, 2, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 100, 250, 500, 2000, 40, 0.25);
   elseif(event.effect_id=="Atomic Weapon hit 4t") then
-	 atomic_weapon_hit(event, 1, 6, 7, 20, 130, 120, 150, 180, 80, 1, true);
+	 atomic_weapon_hit(event, 1, 6, 7, 20, 130, 120, 150, 180, 80, 1, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 120, 300, 900, 4000, 70, 0.4);
   elseif(event.effect_id=="Atomic Weapon hit 8t") then
-	 atomic_weapon_hit(event, 3, 8, 14, 25, 200, 200, 200, 180, 100, 1, true);
+	 atomic_weapon_hit(event, 3, 8, 14, 25, 200, 200, 200, 180, 100, 1, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 150, 400, 1250, 10000, 100, 0.6);
   elseif(event.effect_id=="Atomic Weapon hit 20t") then
-	 atomic_weapon_hit(event, 5, 10, 20, 30, 320, 320, 320, 180, 150, 1, true);
+	 atomic_weapon_hit(event, 5, 10, 20, 30, 320, 320, 320, 180, 150, 1, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 250, 600, 1800, 15000, 160, 1);
   elseif(event.effect_id=="Atomic Weapon hit 500t") then
-	 atomic_weapon_hit(event, 10, 20, 40, 35, 400, 400, 600, 400, 300, 1*settings.global["large-nuke-fire-scaledown"].value, true);
+	 atomic_weapon_hit(event, 10, 20, 40, 35, 400, 400, 600, 400, 300, 1*settings.global["large-nuke-fire-scaledown"].value, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 400, 800, 2500, 25000, 300, 2);
   elseif(event.effect_id=="Atomic Weapon hit 1kt") then
-	 atomic_weapon_hit(event, 20, 40, 80, 75, 800, 800, 1200, 800, 300, 2*settings.global["large-nuke-fire-scaledown"].value, true);
+	 atomic_weapon_hit(event, 20, 40, 80, 75, 800, 800, 1200, 800, 300, 2*settings.global["large-nuke-fire-scaledown"].value, true, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 600, 1200, 8000, 60000, 600, 4);
   elseif(event.effect_id=="Atomic Weapon hit 15kt") then
-	 atomic_weapon_hit(event, 50, 100, 200, 150, 2000/settings.global["large-nuke-blast-range-scaledown"].value, 1000, 4000, 1000, 500, settings.global["huge-nuke-fire-scaledown"].value, false);
+	 atomic_weapon_hit(event, 50, 100, 200, 150, 2000/settings.global["large-nuke-blast-range-scaledown"].value, 1000, 4000, 1000, 500, settings.global["huge-nuke-fire-scaledown"].value, false, true);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 1500, 3000, 20000, 100000, 1500, 8);
   elseif(event.effect_id=="Atomic Weapon hit 100kt") then
-	 atomic_weapon_hit(event, 90, 180, 500, 400, 5500/settings.global["really-huge-nuke-blast-range-scaledown"].value, 2500, 9000, 1500, 1000, 2*settings.global["really-huge-nuke-fire-scaledown"].value, false);
+	 atomic_weapon_hit(event, 90, 180, 500, 400, 5500/settings.global["really-huge-nuke-blast-range-scaledown"].value, 2500, 9000, 1500, 1000, 2*settings.global["really-huge-nuke-fire-scaledown"].value, false, false);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 2700, 5400, 36000, 200000, 2700, 16);
   elseif(event.effect_id=="Atomic Weapon hit 1Mt") then
-	 atomic_weapon_hit(event, 190, 390, 1200, 1000, 12000/settings.global["mega-nuke-blast-range-scaledown"].value, 5000, 10000, 3200, 2500, 0, false);
+	 atomic_weapon_hit(event, 190, 390, 1200, 1000, 12000/settings.global["mega-nuke-blast-range-scaledown"].value, 5000, 10000, 3200, 2500, 0, false, false);
 	 createBlastSoundsAndFlash(event.target_position, game.surfaces[event.surface_index], 6000, 10000, 60000, 400000, 5000, 32);
   elseif(event.effect_id=="Nuke firing") then
 	 nukeFiredScan(event);
@@ -526,6 +554,7 @@ end)
 
 
 script.on_nth_tick(3600, function(event)
+	-- crater filling handling
 	if(global.waitingNukes ==nil) then
 		global.waitingNukes = {}
 	end
