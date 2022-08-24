733097ef79565d1af6cc6c0681832a79f04b2157
Robert <robert.witts.rmw@gmail.com>
2022-08-24 14:40:30 +0100

    Added SE-specific recipes, added some MIRV code

--- True-Nukes_0.3.0/prototypes/nukes/data-nukes.lua
+++ True-Nukes_0.3.0/prototypes/nukes/data-nukes.lua
@@ -4,3 +4,7 @@ require("data-nukes-intermediate")
 require("data-nukes-warheads-create")
 require("data-nukes-warheads-weapon-appearance")
 require("data-nukes-technology")
+
+if mods["space-exploration"] then
+  require("compatibility.SE")
+end
