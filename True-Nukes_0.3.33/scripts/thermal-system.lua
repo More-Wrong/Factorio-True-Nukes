f36c89b0ce88e7aaeec52597a24a89596d0a92e1
Alicia <alicia.spelt.properly@gmail.com>
2023-11-27 12:33:56 +0000

    Fixed issue with nil values in thermal system

--- True-Nukes_0.3.33/scripts/thermal-system.lua
+++ True-Nukes_0.3.33/scripts/thermal-system.lua
@@ -130,6 +130,10 @@ local function atomic_thermal_blast_internal(surface_index, position, force, cau
       end
     end
     if(i<100 and y ~= position.y +thermal_max_r) then
+      distSq1 = distSq1 or ((x-position.x)*(x-position.x)        +(y-position.y)*(y-position.y))
+      distSq2 = distSq2 or ((x+100-position.x)*(x+100-position.x)+(y-position.y)*(y-position.y))
+      distSq3 = distSq3 or ((x-position.x)*(x-position.x)        +(y+100-position.y)*(y+100-position.y))
+      distSq4 = distSq4 or ((x+100-position.x)*(x+100-position.x)+(y+100-position.y)*(y+100-position.y))
       while(x+100<=position.x +thermal_max_r) do
         if(distSq1<thermSq or distSq2<thermSq or distSq3<thermSq or distSq4<thermSq) then
           table.insert(areas, {{x, y}, {x+100, position.y+thermal_max_r}});
